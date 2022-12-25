600709ab633593918c472ade024e90f7d51ed2e5
ackerman <julio.sergio2709@gmail.com>
2022-12-25 16:09:45 -0500

    Add: game history

--- src/page/board.js
+++ src/page/board.js
@@ -1,41 +1,19 @@
 import React from "react";
 import Square from "./square";
-import { useState } from "react";
 
-export default function Board(props) {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-  
-    function handleClick(i) {
-      const newSquares = squares.slice();
-      if (calculateWinner(newSquares) || newSquares[i]) {
-        return;
-      }
-      newSquares[i] = xIsNext ? 'X' : 'O';
-      setSquares(newSquares);
-      setXIsNext(!xIsNext);
-    }
+export default function Board({squares, onClick, xIsNext, ...restProps}) {
   
     function renderSquare(i) {
       return (
         <Square
           value={squares[i]}
-          onClick={() => handleClick(i)}
+          onClick={() => onClick(i)}
         />
       );
     }
-  
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-    }
-  
+
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {renderSquare(0)}
           {renderSquare(1)}
@@ -53,24 +31,4 @@ export default function Board(props) {
         </div>
       </div>
     );
-}
-  
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
 }
--- src/page/index.js
+++ src/page/index.js
@@ -1,16 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./board";
 
 export default function Game() {
+    const [history, setHistory] = useState([{
+        squares: Array(9).fill(null),
+    }]);
+    const [xIsNext, setXIsNext] = useState(true);
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+          `Go to move #${move}` :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+    });
+
+    function handleClick(i) {
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory(
+          history.concat([{
+            squares: squares,
+          }]),
+        );
+        setXIsNext(!xIsNext);
+    }
+
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <div className="game">
           <div className="game-board">
-            <Board/>
+            <Board squares={current.squares} onClick={(i) => handleClick(i)} xIsNext={xIsNext} />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
     );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
 }
