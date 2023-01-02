c2ebee77ccdca501d4e7c8ecb6bb6f252df05bc5
ackerman <julio.sergio2709@gmail.com>
2023-01-01 22:17:47 -0500

    succes fetching cats with axios

--- src/containers/cat-api/index.js
+++ src/containers/cat-api/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderContainer from "../../common/header";
 import Footer from "../../common/footer";
 import { MainContainer, Btn, ContentContainer} from "../../common/styles";
+import { CatImage } from "./styles";
 import axios from "axios";
 
 const api = axios.create({
@@ -9,21 +10,28 @@ const api = axios.create({
 });
 api.defaults.headers.common['X-API-KEY'] = 'live_XTd3skGaIZ4bJVqKnaa6db01fvPZ2JdPqIoGzLMYq5fsi1IVvbKsg2IIkzwm4IKM';
 
-async function getCat() {
-    try {
-      const response = await api.get('/images/search?limit=2');
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-}
 
 export default function CatAPI() {
-
+    const [catsUrl, setCatUrls] = useState(Array(2).fill(''));
+     
+    async function getCat() {
+        try {
+          const {data} = await api.get('/images/search?limit=2');
+          console.log(data);
+          setCatUrls(data.map(
+            cat => {return cat.url}
+          ))
+        } catch (error) {
+          console.error(error);
+        }
+    }
+    
     return (
         <MainContainer direction={"column"}>
             <HeaderContainer direction={"column"}>
                 <ContentContainer direction={"column"} height={"auto"} margin={"auto"} width={"300px"}>
+                    <CatImage src={catsUrl[0]}/>
+                    <CatImage src={catsUrl[1]}/>
                     <Btn onClick={()=>{
                         getCat();
                     }} margin={"auto"}>Get Cat</Btn>
---
+++ src/containers/cat-api/styles/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const CatImage = styled.img`
+    margin: 20px auto;
+    width: 300px;
+    height: 300px;
+`;
