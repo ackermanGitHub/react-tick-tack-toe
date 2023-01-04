ce6a5dd01421e5ddcb875546b6a7c3732f5f7389
ackerman <julio.sergio2709@gmail.com>
2023-01-03 19:41:59 -0500

    All fiexd

--- src/containers/cat-api/index.js
+++ src/containers/cat-api/index.js
@@ -6,15 +6,6 @@ import { CatImage } from "./styles";
 import { api } from "./API";
 //dimport { Mafs, CartesianCoordinates, FunctionGraph } from "mafs";
 
-function isCubaLatitud(latitude, longitude){
-    let result = true;
-    if(latitude > -75 || latitude < -85)
-        result = false;
-    if(longitude > 24 || longitude < 19)
-        result = false;
-    return result;
-}
-
 export default function CatAPI() {
     const [catsUrl, setCatUrls] = useState([]);
     
@@ -26,45 +17,9 @@ export default function CatAPI() {
             console.error(error);
         }
     }
-    /* async function getLocationInfo(latitude, longitude) {
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCAPCfPULsJkPU3rryRLTPvUzNRbLIX_2s`
-        );
-        const data = await response.json();
-        return data.results[0];
-    } */
     
     useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    console.log({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                    });
-                    if(isCubaLatitud(position.coords.latitude, position.coords.longitude))
-                        window.alert("You are in Cuba");
-                    //else
-                    //    getCat();
-              },
-              (error) => {
-                console.error(error);
-              },
-              {
-                enableHighAccuracy: true,
-                timeout: 5000
-              }
-            );
-        } else {
-            console.error('Geolocation is not supported by this browser.');
-        }
-
-        if (navigator.connection) {
-            //console.log(navigator.connection);
-        } else {
-            console.error('Network Information is not supported by this browser.');
-        }
-
+        
     }, [])
 
     return (
@@ -72,14 +27,14 @@ export default function CatAPI() {
             <HeaderContainer direction={"column"}>
                 <ContentContainer direction={"column"} height={"auto"} margin={"auto"} width={"300px"}>
                     {
-                        catsUrl.map((item, index)=>{
+                        [].map((item, index)=>{
                             return (
                                 <CatImage src={item} key={index} />
 
                             )
                         })
                     }
-                    <Btn onClick={console.log("getCatt")/* getCat */} margin={"auto"}>Get Cat</Btn>
+                    <Btn onClick={null} margin={"auto"}>Get Cat</Btn>
                 </ContentContainer>
             </HeaderContainer>
             <Footer></Footer>
