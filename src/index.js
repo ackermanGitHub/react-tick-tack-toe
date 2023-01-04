fef4d9f5914edd59f9ff0a41a605672ee036ef28
ackerman <julio.sergio2709@gmail.com>
2023-01-03 21:11:24 -0500

    tailwind css vs styled-components/mui

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalStyles } from './global-styles';
+//import { GlobalStyles } from './global-styles';
+import './index.css';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <GlobalStyles></GlobalStyles>
     <App />
   </StrictMode>
 );
