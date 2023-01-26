fe654a2c3860488e73ab3844abee8ec5c4e4a285
ackerman <julio.sergio2709@gmail.com>
2023-01-25 23:23:31 -0500

    starting with forms

--- src/containers/testing/index.tsx
+++ src/containers/testing/index.tsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../common/header";
 import Footer from "../../common/footer";
 import { MainContainer, Aside, Btn, ContentContainer } from "./styles/test";
 
 export default function Testing(){
+    const [isLogin, setLogin] = useState(false);
+    const [user, setUser] = useState({
+        userName: "",
+        password: "",
+    })
+
+    const handleSubmit = (e: any) => {
+        console.log(e)
+    }
+    const handleChange = (e: any) => {
+        console.log(e)
+    }
 
     return (
         <MainContainer>
             <Header/>
             <ContentContainer>
-                this is a .tsx file
-                <Btn>🍔</Btn>
+            <form onSubmit={handleSubmit}>
+                <label>
+                Essay:
+                <textarea value={user.userName} onChange={handleChange} />
+                <textarea value={user.password} onChange={handleChange} />
+                <select multiple={true}>
+                    <option value="grapefruit">Grapefruit</option>
+                    <option onChange={(e)=>{console.log(e)}} value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango">Mango</option>
+                </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
             </ContentContainer>
             <Aside></Aside>
             <Footer/>
